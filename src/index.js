b062b3969c0721f44248c990ee4890c62a1bfc3a
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2026-03-16 12:08:15 -0500

    fix: fixed bug

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // Simple demo application
-console.log("Hello from release-please demo!");
+console.log("Hello from release-please demo! by william");
 console.log("Version 1.0.0");
 console.log("This is a simple demo application to demonstrate release-please.");
