ba876b97d105d516a2ee448b8df5664cdf956730
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2026-03-16 12:29:08 -0500

    fix: anoother fix

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ console.log("Hello from release-please demo! by william");
 console.log(
   "This application is used to demonstrate the release-please tool for automated releases.",
 );
+console.log(
+  "This application will print the current version and a message to the console.",
+);
 console.log("Version 1.0.0");
 console.log("This is a simple demo application to demonstrate release-please.");
