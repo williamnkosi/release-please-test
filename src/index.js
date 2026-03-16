65f9abccd4a8eab40bd283bed41acc912b2e1fbc
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2026-03-16 12:07:31 -0500

    chore: added more code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 // Simple demo application
-console.log('Hello from release-please demo!');
-console.log('Version 1.0.0');
+console.log("Hello from release-please demo!");
+console.log("Version 1.0.0");
+console.log("This is a simple demo application to demonstrate release-please.");
